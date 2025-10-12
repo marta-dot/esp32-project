533272325bf7041d3d2539f74a0602b0fa15c3ed
marta-dot <dabrowskam2003@gmail.com>
2025-10-12 19:46:25 +0200

    init

---
+++ main/main.cpp
@@ -0,0 +1,30 @@
+// Please keep these 2 lines at the beginning of each cpp module - tag and local log level
+static const char* LOG_TAG = "Main";
+#define LOG_LOCAL_LEVEL ESP_LOG_INFO
+
+
+#include "defines.h"
+#include "sleep.h"
+
+
+void run(void);
+
+extern "C"
+{
+    void app_main(void)
+    {
+        run();
+    }
+}
+
+void run(void)
+{
+    LOG_INFO("Hello from main!");
+
+    while (1)
+    {
+        LOG_DEBUG("Hello loop");
+        SLEEP_MS(1000);
+    }
+}
+
